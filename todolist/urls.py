14ed7ba2d113cb71e487dac0f66ac601bda582e1
vanchrisx <christianvannoppen@gmail.com>
2024-06-06 10:40:07 -0300

    Task functions ready

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from todolist import views
 
 urlpatterns = [
-    path('tasks/', views.tasks, name='tasks'),
-    path('create-task/', views.create_task, name='create-task')
+    path('tasks/', views.TasksList.as_view(), name='tasks'),
+    path('create-task/', views.create_task, name='create-task'),
+    path('add-task/', views.add_task, name='add-task'),
+    path('complete-task/<int:pk>/', views.complete_task, name='complete-task'),
+    path('delete-task/<int:pk>/', views.delete_task, name='delete-task'),
+    path('all-tasks/', views.all_tasks, name='all_tasks'),
+    path('completed-tasks/', views.completed_tasks, name='completed-tasks'),
+    path('pending-tasks/', views.pending_tasks, name='pending-tasks')
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,12 +1,90 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
 from .form import TaskForm
+from .models import Task
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.decorators.http import require_http_methods
+from django.contrib import messages
 
 # Create your views here.
 
 
-def tasks(request):
-    return render(request, 'tasks.html')
-
-
+@login_required
 def create_task(request):
     return render(request, 'partials/create-task.html', {'form': TaskForm})
+
+
+@login_required
+def add_task(request):
+    try:
+        form = TaskForm(request.POST)
+        new_task = form.save(commit=False)
+        new_task.user = request.user
+        new_task.save()
+        return redirect('tasks')
+    except ValueError:
+        return render(request, 'tasks.html', {
+            'form': TaskForm,
+            'error': 'Complete todos los campos'
+        })
+
+
+class TasksList(LoginRequiredMixin, ListView):
+    template_name = 'tasks.html'
+    model = Task
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+
+@require_http_methods(['DELETE'])
+def delete_task(request, pk):
+    Task.objects.get(pk=pk).delete()
+
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'partials/tasks-list.html', {'tasks': tasks})
+
+
+def complete_task(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.status = True
+    task.save()
+
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'partials/tasks-list.html', {'tasks': tasks})
+
+
+require_http_methods(['GET'])
+
+
+@login_required
+def all_tasks(request):
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'partials/tasks-list.html', {
+        'tasks': tasks
+    })
+
+
+require_http_methods(['GET'])
+
+
+@login_required
+def completed_tasks(request):
+    tasks = Task.objects.filter(user=request.user, status=True)
+    return render(request, 'partials/tasks-list.html', {
+        'tasks': tasks
+    })
+
+
+require_http_methods(['GET'])
+
+
+@login_required
+def pending_tasks(request):
+    tasks = Task.objects.filter(user=request.user, status=False)
+    return render(request, 'partials/tasks-list.html', {
+        'tasks': tasks
+    })
